43555ffb7865735e069143e600795de8a6eb32e9
Simon <simon@aparicio.org>
2020-03-07 19:26:52 +0100

    Initial commit: Create project and a basic folder structure

---
+++ iOSCodeTest/iOSCodeTest/Source/Movies/View/ViewController.swift
@@ -0,0 +1,19 @@
+//  ViewController.swift
+//  iOSCodeTest
+//
+//  Created by Simón Aparicio on 07/03/2020.
+//  Copyright © 2020 iPon.es. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
