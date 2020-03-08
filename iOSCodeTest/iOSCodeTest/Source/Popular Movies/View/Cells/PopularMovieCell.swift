ce7ab0c4cd3fc1e6fdacaf4c23859629ee439552
Simon <simon@aparicio.org>
2020-03-08 17:54:53 +0100

    prepare the cell for the popular movies table

--- iOSCodeTest/iOSCodeTest/Source/Popular Movies/View/Cells/PopularMovieCell.swift
+++ iOSCodeTest/iOSCodeTest/Source/Popular Movies/View/Cells/PopularMovieCell.swift
@@ -11,10 +11,34 @@ import UIKit
 // TODO: The cell protocol for actions to pass data from cell to ViewController if needed
 
 protocol PopularMovieCell {
-    
+    func configure(id: Int, title: String, posterPath: String, releaseDate: String)
 }
 
 class PopularMovieCellImpl: UITableViewCell, PopularMovieCell {
+    
+    // TODO: @IBOulet labels and image
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var overviewTextView: UITextView!
+    
+    
+    // This occurs when the xib is ready
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.contentView.backgroundColor = .white
+    }
+    
+    func configure(id: Int, title: String, posterPath: String, releaseDate: String) {
+        
+        // TODO: asign values to labels and images
+        idLabel.text = String(id)
+        titleLabel.text = title
+        releaseDateLabel.text = releaseDate
+        
+        // TODO: retrieve image from url
+    }
     
 }
 
