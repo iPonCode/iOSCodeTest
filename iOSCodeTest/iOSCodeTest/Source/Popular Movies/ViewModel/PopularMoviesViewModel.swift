3d8b0e9c6d200e08d63c76dbcd98b5a7b653d25d
Simon <simon@aparicio.org>
2020-03-08 14:04:40 +0100

    build poster image and popular movies urls

--- iOSCodeTest/iOSCodeTest/Source/Popular Movies/ViewModel/PopularMoviesViewModel.swift
+++ iOSCodeTest/iOSCodeTest/Source/Popular Movies/ViewModel/PopularMoviesViewModel.swift
@@ -22,14 +22,7 @@ class PopularMoviesViewModelImpl: PopularMoviesViewModel {
     func retrievePopularMovies() {
         print("retrieving popular movies from Webservice..")
         
-        let baseUrl = "https://api.themoviedb.org/3/movie/popular"
-        let apiKey = "72528dfa50871fd94e3094b9d73cd3be"
-        // Options parameters
-        let language = "es-ES"  //ISO 639-1 value to display translated data for supported fields
-        let page = "1"          //from 1 to 1000
-        let region = "ES"       //ISO 3166-1 code to filter release dates, uppercase
-        
-        let url = baseUrl + "?api_key=" + apiKey + "&language=" + language + "&page=" + page + "&region=" + region
+        let url = buildPopularMoviesUrl()
         AF.request(url).responseJSON {[weak self] response in
             
             guard let serverData = response.data, let popularMovies = try? JSONDecoder().decode(PopularMovies.self, from: serverData) else {
@@ -39,7 +32,32 @@ class PopularMoviesViewModelImpl: PopularMoviesViewModel {
             
             self?.movies = popularMovies.results
             print(self?.movies ?? "NO MOVIES")
+            
+            }
         }
+    
+    func buildPopularMoviesUrl() -> String {
+        
+        // TODO: put all this info like apiKey in a Constant sturct
+        let baseUrl = "https://api.themoviedb.org/3/movie/popular"
+        let apiKey = "72528dfa50871fd94e3094b9d73cd3be"
+        
+        // Options parameters
+        let language = "es-ES"  //ISO 639-1 value to display translated data for supported fields
+        let page = "1"          //from 1 to 1000
+        let region = "ES"       //ISO 3166-1 code to filter release dates, uppercase
+        
+        return baseUrl + "?api_key=" + apiKey + "&language=" + language + "&page=" + page + "&region=" + region
+    }
+    
+    func buildImagedUrl(_ path: String) -> String {
+        
+        // path includes the / like this: /ya9ojfuWylP4P6iiaIaxz67Chu0.jpg
+        // TODO: get this configuration from webservice and cache
+        let baseUrl = "https://image.tmdb.org/t/p/"
+        let posterSize = "w92" // TODO: Create enum with all sizes and pass through as a parameter
+        
+        return baseUrl + posterSize + path
     }
         
 }
