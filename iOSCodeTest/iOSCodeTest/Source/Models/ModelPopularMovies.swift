07ad522450698a0aef910bc9e9384579d5474754
Simon <simon@aparicio.org>
2020-03-08 02:51:54 +0100

    Model to get popular movies

---
+++ iOSCodeTest/iOSCodeTest/Source/Models/ModelPopularMovies.swift
@@ -0,0 +1,59 @@
+//  ModelPopularMovies.swift
+//  iOSCodeTest
+//
+//  Created by Simón Aparicio on 08/03/2020.
+//  Copyright © 2020 iPon.es. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - PopularMovies
+struct PopularMovies: Codable {
+    let page, totalResults, totalPages: Int
+    let results: [PopularMovie]
+
+    enum CodingKeys: String, CodingKey {
+        case page
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+        case results
+    }
+}
+
+// MARK: - PopularMovie
+struct PopularMovie: Codable {
+    let popularity: Double
+    let voteCount: Int
+    let video: Bool
+    let posterPath: String
+    let id: Int
+    let adult: Bool
+    let backdropPath: String
+    let originalLanguage: OriginalLanguage
+    let originalTitle: String
+    let genreIDS: [Int]
+    let title: String
+    let voteAverage: Double
+    let overview, releaseDate: String
+
+    enum CodingKeys: String, CodingKey {
+        case popularity
+        case voteCount = "vote_count"
+        case video
+        case posterPath = "poster_path"
+        case id, adult
+        case backdropPath = "backdrop_path"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case genreIDS = "genre_ids"
+        case title
+        case voteAverage = "vote_average"
+        case overview
+        case releaseDate = "release_date"
+    }
+}
+
+enum OriginalLanguage: String, Codable {
+    case en = "en"
+    case ko = "ko"
+}
