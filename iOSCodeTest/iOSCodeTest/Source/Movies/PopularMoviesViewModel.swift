5089f7c14e99741d688402d3fcb7a28a3b14afd5
Simon <simon@aparicio.org>
2020-03-08 05:54:36 +0100

    popular movies webservice working

--- iOSCodeTest/iOSCodeTest/Source/Movies/PopularMoviesViewModel.swift
+++ iOSCodeTest/iOSCodeTest/Source/Movies/PopularMoviesViewModel.swift
@@ -16,9 +16,30 @@ protocol PopularMoviesViewModel {
 // Here all business logic
 class PopularMoviesViewModelImpl: PopularMoviesViewModel {
     
+    var movies: [PopularMovie] = []
+    
     // The Webservice call
     func retrievePopularMovies() {
         print("retrieving popular movies from Webservice..")
+        
+        let baseUrl = "https://api.themoviedb.org/3/movie/popular"
+        let apiKey = "72528dfa50871fd94e3094b9d73cd3be"
+        // Options parameters
+        let language = "es-ES"  //ISO 639-1 value to display translated data for supported fields
+        let page = "1"          //from 1 to 1000
+        let region = "ES"       //ISO 3166-1 code to filter release dates, uppercase
+        
+        let url = baseUrl + "?api_key=" + apiKey + "&language=" + language + "&page=" + page + "&region=" + region
+        AF.request(url).responseJSON {[weak self] response in
+            
+            guard let serverData = response.data, let popularMovies = try? JSONDecoder().decode(PopularMovies.self, from: serverData) else {
+                print("Error decoding server response")
+                return
+            }
+            
+            self?.movies = popularMovies.results
+            print("popular movies received: \(String(describing: self?.movies.count))")
+        }
     }
     
 }
